9d4635459fecb79ffc3083efd55e5489d661bc8e
Papa Kofi Akoto Asante <48448418+papakofifog@users.noreply.github.com>
2023-01-22 17:01:07 +0000

    Google authentication underway

--- fronEnd/assets/js/googleAuth.js
+++ fronEnd/assets/js/googleAuth.js
@@ -1,2 +1,27 @@
-    
+import {} from './handleRequest'
+
+gapi.load('auth2', function() {
+    gapi.auth2.init({
+        client_id: 'YOUR_CLIENT_ID',
+        scope: 'https://www.googleapis.com/auth/userinfo.email'
+    });
+});
+
+document.querySelector('signin-button').addEventListener('click', function(){
+    gapi.auth2.getAuthInstance().signIn();
+})
+
+gapi.auth2.getAuthInstance().isSignedIn.listen(function(isSignedIn) {
+    if (isSignedIn) {
+        let authResponse = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse();
+        let code = authResponse.code;
+        // send the code to your server to exchange it for an access token
+
+
+    }
+
+});
+
+function
+
     
