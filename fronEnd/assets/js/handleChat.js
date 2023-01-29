44cd934c3f89af70a85d4cf546cd01c597f8740d
Papa Kofi Akoto Asante <48448418+papakofifog@users.noreply.github.com>
2023-01-29 01:38:01 +0000

    new ui design accomplished it is left with functionality

--- fronEnd/assets/js/handleChat.js
+++ fronEnd/assets/js/handleChat.js
@@ -1,12 +1,52 @@
 import { sendData } from "./handleRequest.js";
 import { showMessage } from "./userhomeMessages.js";
+import { formatActiveUserFriends } from "./userHome.js";
 const userContactsContainer= document.querySelector('#userContactsList');
 const chatWithFriendIcon= document.querySelector('#chatFriendIcon');
 const chatWithUserUsername= document.querySelector('#chat-name');
 const chatsView = document.querySelector('#messages');
 let currentUser= document.querySelector('#userEverything')
 let chatProfileHeader= document.querySelector('#chatProfile-user');
+let personalFriends= document.querySelector("#personal-friends");
+let groups=document.querySelector("#groups");
+let friendItem1= document.querySelector('#friendItem-1');
+let friendItem2= document.querySelector('#friendItem-2');
+let userContacts=document.querySelector('#userContactsList');
 
+
+function handleChatView(value1,value2){
+    friendItem1.addEventListener('click',async function(){
+        userContacts.innerHTML=value1;
+    })
+
+    friendItem2.addEventListener('click', function(){
+        userContacts.innerHTML=value2;
+    })
+}
+
+ async function MonitorOptions (){
+    let friends= await formatActiveUserFriends();
+    // show the friends chat view when the page loads.
+    handleChatView(friends,"");
+
+
+    // handdle chat behaviour when a user interact with the chat app.
+    personalFriends.addEventListener('click', async function(){
+        friendItem1.innerHTML="Friends";
+        friendItem2.innerHTML="New Contacts";
+        let friends= await formatActiveUserFriends();
+        userContacts.innerHTML=friends;
+        handleChatView(friends,"");
+    })
+    groups.addEventListener('click', function (){
+        friendItem1.innerHTML="Groups";
+        friendItem2.innerHTML="New Groups";
+        let groups="";
+        userContacts.innerHTML=""
+        handleChatView("","")
+    })
+    
+}
 
 
 function MonitorClickFriend(){
@@ -28,6 +68,7 @@ function MonitorClickFriend(){
 
         })
     })
+
     
     
 }
@@ -55,5 +96,5 @@ function walkthroughMessages(data){
     }
 }
 
-export {MonitorClickFriend}
+export {MonitorClickFriend , MonitorOptions }
 
