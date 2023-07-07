d62a6f19457585539b2ae4619216507667d9253e
paakofifog <paakofi275@gmail.com>
2023-07-07 21:45:20 +0000

    was able to connect to a mongoose local server

--- zipiApi/Settings/connectToDb.js
+++ zipiApi/Settings/connectToDb.js
@@ -1,10 +1,11 @@
 const mongoose= require('mongoose')
 require('dotenv').config()
 
+const url="mongodb://127.0.0.1:27017/zipiChat";
+
 const connectToZipiDB= async()=>{
     try{
-        let dbConnection= await mongoose.connect(process.env['DB_SECRET'],
-        {useNewUrlParser: true,useUnifiedTopology: true});
+        let dbConnection= await mongoose.connect(url,{});
         if(connectToZipiDB){
             console.log("Connected to the database Successfully")
             return dbConnection;
